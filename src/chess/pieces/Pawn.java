9022f8db186e285bae835bd1d198a287476a4794
Felipe Vedoveto <vedoveto99@gmail.com>
2020-01-27 18:34:11 -0300

    Fixing pawn moving bug

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -44,7 +44,7 @@ public class Pawn extends ChessPiece {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 
-			p.setValues(position.getRow() - 1, position.getColumn() - 1);
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
@@ -78,7 +78,7 @@ public class Pawn extends ChessPiece {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 
-			p.setValues(position.getRow() + 1, position.getColumn() - 1);
+			p.setValues(position.getRow() + 1, position.getColumn() + 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
